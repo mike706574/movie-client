5b76e155a0d3fe563a752ae30ab8f0a88767e645
mike <mike706574@gmail.com>
2017-02-26 14:36:32 -0600

    Handle root and 404.

--- project.clj
+++ project.clj
@@ -7,10 +7,11 @@
             :comments "same as Clojure"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [com.taoensso/timbre "4.8.0"]
+                 [ring/ring-jetty-adapter "1.5.1"]
                  [environ "1.1.0"]
-                 [org.clojure/clojurescript "1.9.456"]
+                 [org.clojure/clojurescript "1.9.494"]
                  [reagent "0.6.0"]
                  [re-frame "0.9.2"]
                  [day8.re-frame/http-fx "0.1.3"]
@@ -20,28 +21,36 @@
             [org.clojure/tools.nrepl "0.2.12"]
             [lein-figwheel "0.5.9"]]
   :source-paths ["src/clj"]
-  :aot :all
-  :uberjar-name "movie-client.jar"
-  :main movie-client.server
   :hooks [leiningen.cljsbuild]
-  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
-                                  [figwheel-sidecar "0.5.0-2"]]
+  :profiles {:dev {:source-paths ["dev"]
+                   :target-path "target/dev"
+                   :dependencies [;[clj-http "3.4.1"]
+                                  [org.clojure/tools.namespace "0.2.11"]
+                                  [com.cemerick/piggieback "0.2.1"]
+                                  [figwheel-sidecar "0.5.9"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :cljsbuild
-                   {:builds {:client {:figwheel {:on-jsload "movie-client.core/run"
-                                                 :websocket-host "192.168.1.141"}
-                                      :compiler {:main "movie-client.core"
-                                                 :asset-path "js"
-                                                 :optimizations :none
-                                                 :closure-defines {movie-client.core/api-uri "http://192.168.1.141:8000"}
-                                                 :source-map true
-                                                 :source-map-timestamp true}}}}}
-             :production {:cljsbuild
-                          {:builds {:client {:compiler {:output-dir "target"
-                                                        :optimizations :advanced
-                                                        :elide-asserts true
-                                                        :closure-defines {movie-client.core/api-uri "https://mike-movie-server.herokuapp.com"}
-                                                        :pretty-print false}}}}}}
+                   {:builds
+                    {:client
+                     {:figwheel {:on-jsload "movie-client.core/run"
+                                 :websocket-host "192.168.1.141"}
+                      :compiler {:main "movie-client.core"
+                                 :asset-path "js"
+                                 :optimizations :none
+                                 :closure-defines {movie-client.core/api-uri "http://192.168.1.141:8000"}
+                                 :source-map true
+                                 :source-map-timestamp true}}}}}
+             :production {:aot :all
+                          :main movie-client.server
+                          :uberjar-name "movie-client.jar"
+                          :cljsbuild
+                          {:builds
+                           {:client
+                            {:compiler {:output-dir "target"
+                                        :optimizations :advanced
+                                        :elide-asserts true
+                                        :closure-defines {movie-client.core/api-uri "https://mike-movie-server.herokuapp.com"}
+                                        :pretty-print false}}}}}}
   :figwheel {:repl false}
   :clean-targets ^{:protect false} ["resources/public/js"]
   :cljsbuild {:builds {:client {:source-paths ["src/cljs"]
--- src/clj/movie_client/server.clj
+++ src/clj/movie_client/server.clj
@@ -1,18 +1,73 @@
 (ns movie-client.server
   (:gen-class :main true)
-  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
-            [compojure.handler :refer [site]]
-            [compojure.route :refer [not-found]]
+  (:require [com.stuartsierra.component :as component]
             [environ.core :refer [env]]
             [ring.adapter.jetty :as jetty]
-            [ring.util.response :refer [file-response]]
-            [ring.middleware.resource :refer [wrap-resource]]))
+            [ring.util.response :refer [resource-response file-response not-found]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [taoensso.timbre :as log]))
 
-(defroutes app
-  (GET "/" [] (file-response "index.html" {:root "public"}))
-  (ANY "*" [] (not-found (file-response "404.html" {:root "public"}))))
+(defn root
+  [request]
+  (if (= (:uri request) "/")
+    (resource-response "index.html" {:root "public"})
+    (resource-response "404.html" {:root "public"})))
+
+(def handler
+  (-> root
+      (wrap-resource "public")))
+
+(defn- already-started
+  [{:keys [id port] :as service}]
+  (log/info (str "Service " id " already started on port " port "."))
+  service)
+
+(defn- start-service
+  [{:keys [id port handler] :as service} handler]
+  (log/info (str "Starting " id " on port " port "..."))
+  (try
+    (let [server (jetty/run-jetty handler {:port port
+                                           :join? false})]
+      (log/info (str "Finished starting."))
+      (assoc service :server server))
+    (catch java.net.BindException e
+      (throw (ex-info (str "Port " port " is already in use.") {:id id
+                                                                :port port})))))
+
+(defn- stop-service
+  [{:keys [id port server] :as service}]
+  (log/info (str "Stopping " id " on port " port "..."))
+  (.stop server)
+  (dissoc service :server))
+
+(defn- already-stopped
+  [{:keys [id] :as service}]
+  (log/info (str id " already stopped."))
+  service)
+
+(defrecord JettyService [id port handler server]
+  component/Lifecycle
+  (start [this]
+    (if server
+      (already-started this)
+      (start-service this handler)))
+  (stop [this]
+    (if server
+      (stop-service this)
+      (already-stopped this))))
+
+(defn jetty-service
+  [{:keys [port] :as config} handler]
+  {:pre [(integer? port)
+         (> port 0)]}
+  (component/using (map->JettyService (assoc config :handler handler)) []))
+
+(defn system
+  [config]
+  {:app (jetty-service config handler)})
 
 (defn -main
   [& [port]]
-  (let [port (Integer. (or port (env :port) 5001))]
-    (jetty/run-jetty (site (wrap-resource #'app "public")) {:port port :join? false})))
+  (component/start-system
+   (system {:id "movie-client"
+            :port (Integer. (or port (env :port) 5001))})))
